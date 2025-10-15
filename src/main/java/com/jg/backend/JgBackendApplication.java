1a41a2b39cfe5a91120dd3f6583de3144cd930d2
Brayan Guerrero <brayanguerrero.danderino@earlywarning.com>
2025-10-15 09:14:20 -0600

    first commit

---
+++ src/main/java/com/jg/backend/JgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jg.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jg/backend/JgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
